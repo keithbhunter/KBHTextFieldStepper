c1f9ed94b91093fe1665165205b7a1adfeab4b0b
Keith Hunter <keith.hunter@outlook.com>
2015-08-10 20:19:56 -0400

    Recreated UIStepper UI.

--- Example/KBHTextFieldStepper/KBHTextFieldStepper/KBHTextFieldStepper.swift
+++ Example/KBHTextFieldStepper/KBHTextFieldStepper/KBHTextFieldStepper.swift
@@ -8,9 +8,41 @@
 
 import UIKit
 
-class KBHTextFieldStepper: UIControl {
+//  + and - button are 47 x 29
 
+class KBHTextFieldStepper: UIControl {
+    
+    // MARK: - Initializers
+    
+    init() {
+        let theFrame = CGRectMake(0, 0, 94, 29)
+        super.init(frame: theFrame)
+        self.setup()
+    }
+    
+    override init(frame: CGRect) {
+        let theFrame = CGRectMake(frame.origin.x, frame.origin.y, 94, 29)
+        super.init(frame: theFrame)
+        self.setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.setup()
+    }
+    
+    private func setup() {
+        self.addSubview(KBHTextFieldStepperButton(origin: CGPointMake(0, 0), type: .Minus))
+        self.addSubview(KBHTextFieldStepperButton(origin: CGPointMake(47, 0), type: .Plus))
+        self.backgroundColor = .whiteColor()
+    }
+
+    
+    // MARK: - Drawing
+    
     override func drawRect(rect: CGRect) {
+        super.drawRect(rect)
+        
         self.tintColor.setStroke()
         self.tintColor.setFill()
         
@@ -22,4 +54,53 @@ class KBHTextFieldStepper: UIControl {
         divider.fill()
     }
 
+}
+
+
+private enum KBHTextFieldStepperButtonType {
+    case Plus, Minus
+}
+
+private class KBHTextFieldStepperButton: UIControl {
+    
+    var type: KBHTextFieldStepperButtonType
+    
+    
+    // MARK: - Initializers
+    
+    required init(origin: CGPoint = CGPointMake(0, 0), type: KBHTextFieldStepperButtonType) {
+        self.type = type
+        super.init(frame: CGRectMake(origin.x, origin.y, 47, 29))
+        self.backgroundColor = .clearColor()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: - Drawing
+    
+    private override func drawRect(rect: CGRect) {
+        self.tintColor.setFill()
+        
+        if self.type == .Minus {
+            self.drawMinus()
+        } else {
+            self.drawPlus()
+        }
+    }
+    
+    private func drawMinus() {
+        let minus = UIBezierPath(rect: CGRectMake(15.6667, 14.5, 15.6667, 1))
+        minus.fill()
+    }
+    
+    private func drawPlus() {
+        let horiz = UIBezierPath(rect: CGRectMake(15.6667, 14.5, 15.6667, 1))
+        horiz.fill()
+        let vert = UIBezierPath(rect: CGRectMake(23.5, 6.6665, 1, 15.6667))
+        vert.fill()
+    }
+    
 }
--- Example/KBHTextFieldStepper/KBHTextFieldStepper/ViewController.swift
+++ Example/KBHTextFieldStepper/KBHTextFieldStepper/ViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stepper: UIStepper!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
     }
-
 
 }
 
